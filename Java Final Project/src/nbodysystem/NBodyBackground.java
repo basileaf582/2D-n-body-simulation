9f115ec6c9e72dd61d482a803afdcd8be2bf4a9b
basileaf <97647930+basillea@users.noreply.github.com>
2022-04-24 20:04:09 -0500

    Fixed issues of skipping and preserving velocity

--- Java Final Project/src/nbodysystem/NBodyBackground.java
+++ Java Final Project/src/nbodysystem/NBodyBackground.java
@@ -1,9 +1,6 @@
 package nbodysystem;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
-
 import QuadTree.QuadTree;
 import QuadTree.Region;
 
@@ -54,10 +51,22 @@ public class NBodyBackground {
 		return bodyList.get(spot).getMass();
 	}
 	
+	private double getBodyXVelocity(int spot) {
+		return bodyList.get(spot).getXVelocity();
+	}
+	
+	private double getBodyYVelocity(int spot) {
+		return bodyList.get(spot).getYVelocity();
+	}
+	
 	public void addtoList(Body body) {
 		bodyList.add(body);
 		//System.out.println(bodyList.toString());
 		
+	}
+	
+	public int returnListSize() {
+		return bodyList.size();
 	}
 	
 	public void calculateNextPosition() {
@@ -65,35 +74,74 @@ public class NBodyBackground {
 			for(Body b : bodyList) {
 				xAccelerationNew = 0;
 				yAccelerationNew = 0;
-				xVelocityNew = 0;
-				yVelocityNew = 0;
+				xVelocityNew = b.getXVelocity();
+				yVelocityNew = b.getYVelocity();
 				for(Body z : bodyList) {
 					if(z.equals(b)) {
-						bodyList.get(bodyList.indexOf(z) + 1); //skips to next neighbor;
+						continue; //https://stackoverflow.com/questions/11160952/goto-next-iteration-in-for-loop-in-java#:~:text=If%20you%20want%20to%20skip,(another%20question%20about%20label).
+						//z = bodyList.get((bodyList.indexOf(z) + 1) % bodyList.size()); //skips to next neighbor;
 					}
-					else {
 						//xAccelerationNew = (-G*z.getMass())*(((b.getX() - z.getX()))/(Math.pow((Math.abs((b.getX() - z.getX()))),2)));
 						xAccelerationNew = (-G*z.getMass())*(((b.getX() - z.getX()))/(Math.abs(Math.pow(b.getX() - z.getX(), 2) + Math.pow(b.getY() - z.getY(), 2))));
 						System.out.println(xAccelerationNew);
 						yAccelerationNew = (-G*z.getMass())*(((b.getY() - z.getY()))/(Math.abs(Math.pow(b.getX() - z.getX(), 2) + Math.pow(b.getY() - z.getY(), 2))));
 						System.out.println(yAccelerationNew);
-						xVelocityNew = xAccelerationNew*secperframe;
+						xVelocityNew = b.getXVelocity() + xAccelerationNew*secperframe;
 						System.out.println(xVelocityNew);
-						yVelocityNew = xAccelerationNew*secperframe;
+						yVelocityNew = b.getYVelocity() + yAccelerationNew*secperframe;
 						System.out.println(yVelocityNew);
 						b.setXVelocity(xVelocityNew);
 						b.setYVelocity(yVelocityNew);
 						b.setX(b.getX() + b.getXVelocity());
 						b.setY(b.getY() + b.getYVelocity());
 						System.out.println(b.getX() + " " + b.getY());
-					}
 				}
-				
 			}
+		}
+		else if(bodyList.size() == 1) {
+			getBody(0).setX(getBodyX(0) + getBodyXVelocity(0));
+			System.out.println(getBodyXVelocity(0));
+			getBody(0).setY(getBodyY(0) + getBodyYVelocity(0));
+			System.out.println(getBodyYVelocity(0));
+		}
+	}
+			
+	
+	/*	public void calculateNextPosition() {
+		if(bodyArray.length > 1) {
+			for(int i = 0; i < bodyArray.length + 1; i++) {
+				xAccelerationNew = 0;
+				yAccelerationNew = 0;
+				xVelocityNew += bodyArray[i].getXVelocity();
+				yVelocityNew += bodyArray[i].getYVelocity();
+				for(int j = 0; j < bodyArray.length + 1; j++) {
+					if(bodyArray[j].equals(bodyArray[i])) {
+						j = ((j+1) % bodyArray.length + 1);
+					}
+					//xAccelerationNew = (-G*z.getMass())*(((b.getX() - z.getX()))/(Math.pow((Math.abs((b.getX() - z.getX()))),2)));
+					xAccelerationNew = (-G*bodyArray[j].getMass())*(((bodyArray[i].getX() - bodyArray[j].getX()))/(Math.abs(Math.pow(bodyArray[i].getX() - bodyArray[j].getX(), 2) + Math.pow(bodyArray[i].getY() - bodyArray[j].getY(), 2))));
+					System.out.println(xAccelerationNew);
+					yAccelerationNew = (-G*bodyArray[j].getMass())*(((bodyArray[i].getY() - bodyArray[j].getY()))/(Math.abs(Math.pow(bodyArray[i].getX() - bodyArray[j].getX(), 2) + Math.pow(bodyArray[i].getY() - bodyArray[j].getY(), 2))));
+					System.out.println(yAccelerationNew);
+					xVelocityNew = xAccelerationNew*secperframe;
+					System.out.println(xVelocityNew);
+					yVelocityNew = xAccelerationNew*secperframe;
+					System.out.println(yVelocityNew);
+					bodyArray[i].setXVelocity(xVelocityNew);
+					bodyArray[i].setYVelocity(yVelocityNew);
+					bodyArray[i].setX(bodyArray[i].getX() + bodyArray[i].getXVelocity());
+					bodyArray[i].setY(bodyArray[i].getY() + bodyArray[i].getYVelocity());
+					System.out.println(bodyArray[i].getX() + " " + bodyArray[i].getY());
+					
+				}
+			}
+		}
+		else {
 			
 		}
-		
 	}
+	 * 
+	 */
 	
 	public void debugCalculuate() {
 		for(Body b : bodyList) {
