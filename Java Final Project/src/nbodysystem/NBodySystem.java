6a6193b4f1ddbfe7d82dd18204c319883c758e74
basileaf <97647930+basileaf582@users.noreply.github.com>
2022-05-02 17:40:53 -0500

    Update NBodySystem.java

--- Java Final Project/src/nbodysystem/NBodySystem.java
+++ Java Final Project/src/nbodysystem/NBodySystem.java
@@ -15,14 +15,15 @@ public class NBodySystem extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice(); //https://stackoverflow.com/questions/40320199/how-to-automatically-resize-windows-in-javafx-for-different-resolutions
-			int width = gd.getDisplayMode().getWidth();
-			int height = gd.getDisplayMode().getHeight();
+		//	GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice(); //https://stackoverflow.com/questions/40320199/how-to-automatically-resize-windows-in-javafx-for-different-resolutions
+		//	int width = gd.getDisplayMode().getWidth() - 750;
+		//	int height = gd.getDisplayMode().getHeight() - 550;
 			//double height = Screen.getPrimary().getBounds().getHeight();   
 			//double width = Screen.getPrimary().getBounds().getWidth();  
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("NBodySystemGUI.fxml"));
-			Scene scene = new Scene(root, width - 20, height - 80);
+			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
+			primaryStage.setMaximized(true);
 			primaryStage.show();
 			root.requestFocus();
 		} catch(Exception e) {
